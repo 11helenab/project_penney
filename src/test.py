99ed06b63853015f1a042819b72be10d7df361a8
11helenab <124002641+11helenab@users.noreply.github.com>
2026-02-15 16:51:59 -0500

    def create_deck/def save_deck

--- src/test.py
+++ src/test.py
@@ -1,2 +1,12 @@
-#test file to make sure that this works
-#testing testing 123, when I push this can you see it
+
+def create_deck() -> np.array:
+    'return an array that represents a deck of cards'
+    deck = np.repeat([0,1], [26, 26], axis=0)
+    random.shuffle(deck)
+    return deck
+ 
+def save_deck(deck: np.array) -> None:
+    'save the array to an output folder'
+    folder = 'project_penney/data'
+    file_path = os.path.join(folder, 'card_decks.npy')
+    np.save(file_path, deck)
