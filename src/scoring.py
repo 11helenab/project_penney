5e2047164c923f13996ff9281aa4f0ec63c60076
mgestes <molly.g.estes@gmail.com>
2026-02-23 12:07:47 -0500

    Update scoring.py
    fix bugs in rons method

--- src/scoring.py
+++ src/scoring.py
@@ -4,30 +4,35 @@ def trick_method(choice_a, choice_b, n_decks):
     ''' 
     placeholder function for the trick method
     '''
-    return
+    return 0, 0, 0 # wins_a, wins_b, ties
 
 def rons_method(deck: np.ndarray, choice_a: str, choice_b: str):
     '''
     Ron's scoring method:
     Player's win all upturned cards when their sequence appears
+    Input:
+        deck: 1D np.array of 0s and 1s representing cards
+        choice_a: 3-bit string for Player 1
+        choice_b: 3-bit string for Player 2 (the user)
+    Returns:
+        total scores for each player for the deck.
     '''
-    deck = list(deck)
+    deck = deck.tolist() # convert the deck to a list
     upturned = []
-    rscore_a = 0
-    rscore_b = 0
+    score_a, score_b = 0, 0
 
     while len(deck) > 0:
         # flip a card
         card = deck.pop(0)
-        upturned.append(card)
+        upturned.append(str(card)) # ensure card is a string
 
         # only check once there are 3 cards
         if len(upturned) >= 3:
             last_three = ''.join(str(i) for i in upturned[-3:])
             if last_three == choice_a:
-                rscore_a += len(upturned)
+                score_a += len(upturned)
                 upturned = []
             elif last_three == choice_b:
-                rscore_b += len(upturned)
+                score_b += len(upturned)
                 upturned = []
-    return rscore_a, rscore_b
+    return score_a, score_b
