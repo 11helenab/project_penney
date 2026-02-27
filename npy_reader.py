3722748b8c27864f7b5b6fa9d688b06107f03c22
11helenab <124002641+11helenab@users.noreply.github.com>
2026-02-27 10:59:49 -0500

    allow pickles for the npy reader

--- npy_reader.py
+++ npy_reader.py
@@ -1,4 +1,7 @@
 import numpy as np
 
-data = np.load(r'data\all_decks.npy')
+data = np.load(r'data\all_decks.npy', allow_pickle=True)
+print(data)
+
+data = np.load(r'data\combined_results.npy', allow_pickle=True)
 print(data)
